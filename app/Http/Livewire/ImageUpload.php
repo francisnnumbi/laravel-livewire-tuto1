5452b314c5ca76bc4bdc95c193445c395336a8a5
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-10-02 18:11:22 +0200

    working on uploading and previewing photos

---
+++ app/Http/Livewire/ImageUpload.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Facades\Storage;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class ImageUpload extends Component
+{
+    use WithFileUploads;
+
+    /*
+     * @var \Livewire\TemporaryUploadedFile
+     * */
+    public $photo = [];
+
+    public function save(){
+        $this->validate([
+            'photo.*'=>'image|max:1024', // 1MB Max
+        ]);
+
+        foreach ($this->photo as $im){
+            $im->store('public');// the image will be stored with temporary name
+        }
+
+        // if you want to keep original name
+       // $this->image->storeAs('public', $this->image->getClientOriginalName());
+
+    }
+
+    public function render()
+    {
+        return view('livewire.image-upload', [
+            'images'=> collect(Storage::files('public'))
+            ->filter(fn($file) => in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), ['png', 'jpg', 'jpeg', 'gif', 'webp']))
+                ->map(fn($file) => Storage::url($file)),
+        ]);
+    }
+}
